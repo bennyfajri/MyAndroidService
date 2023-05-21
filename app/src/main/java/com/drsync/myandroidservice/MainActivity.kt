9343980b9a73e95014a5dad8e320d54110bf7cc6
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2023-05-21 23:19:24 +0700

    add Foreground service

--- app/src/main/java/com/drsync/myandroidservice/MainActivity.kt
+++ app/src/main/java/com/drsync/myandroidservice/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.drsync.myandroidservice
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.drsync.myandroidservice.backgroundservice.BackgroundService
 import com.drsync.myandroidservice.databinding.ActivityMainBinding
+import com.drsync.myandroidservice.foregroundservice.ForegroundService
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val bgServiceIntent = Intent(this, BackgroundService::class.java)
+        val foregroundServiceIntent = Intent(this, ForegroundService::class.java)
         binding.apply {
             btnStartBackgroundService.setOnClickListener {
                 startService(bgServiceIntent)
@@ -23,7 +26,17 @@ class MainActivity : AppCompatActivity() {
             btnStopBackgroundService.setOnClickListener {
                 stopService(bgServiceIntent)
             }
+            btnStartForegroundService.setOnClickListener {
+                if(Build.VERSION.SDK_INT >= 26) {
+                    startForegroundService(foregroundServiceIntent)
+                } else {
+                    startService(foregroundServiceIntent)
+                }
+            }
+            btnStopForegroundService.setOnClickListener {
+                stopService(foregroundServiceIntent)
+            }
         }
-        
+
     }
 }
